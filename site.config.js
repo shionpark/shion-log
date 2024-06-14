30d1df4c6e2a460724ddfe0c91315b431659e533
Shion <shionpark@gmail.com>
2024-06-14 13:26:14 +0900

    chore: 블로그 세팅

--- site.config.js
+++ site.config.js
@@ -12,18 +12,18 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `shion-log`,
+      href: "https://github.com/shionpark/shion-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "shion-log",
+    description: "welcome to shion-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://shion-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
